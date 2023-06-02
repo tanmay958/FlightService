a6dfd18635fa7b5db13b92aa065df53977161146
tanmay958 <studytime958@gmail.com>
2023-06-02 22:24:06 +0530

    sequelize and other dependecies for it

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const express =  require("express");
 const {PORT} =  require('./config/ServerConfig');
+
+const bodyParser  =  require("body-parser");
+
+
 const  setupAndStartServer =  async()=>{
     const app = express();
+    app.use(bodyParser.json);
+    app.use(bodyParser.urlencoded({extended:true}));
     const PORT = 3000;
       
     app.listen(PORT,()=>{
