e32b026c69517ebfec67ac6385cb66e592f895c7
tanmay958 <studytime958@gmail.com>
2023-06-03 17:05:40 +0530

    completed city repository

--- src/respository/city-repository.js
+++ src/respository/city-repository.js
@@ -1,29 +1,49 @@
 const {City} =  require("../models/index");
 
 class CityRepository{
-    async createCity({name})
-    {
+    async createCity({name}){
+        try
+      {  const city = await City.create({
+            name:name,// can also be written as name only
+        })
+        return city;}
+        catch(error)
+        {
+            console.log("something happenend in the repo layer");
+            throw{ error};
+        }
+    }
+    async deleteCity({cityId}){
         try{
-            const city =  await City.create({name});
+            await City.destror({
+                where  :{
+                    id :  cityId,
+                }
+            });
         }
         catch(error)
         {
-            throw {error};
+            console.log("something happenend in the repo layer");
+            throw{ error};
         }
     }
-    async deleteCity({cityid})
-    {
-        try{
-            await City.destroy(
-                {
-                    where:{
-                        id :cityid
-                    }
-                });
 
+    async updateCity({id,name}){
+        await City.update({name:name},{
+            where : {
+                id : id,
+            }
+        })
+
+    }
+    async getCity({cityId}){
+        try{
+            const city = await City.findByPk(cityId);
         }
-        catch(err){
-            throw {error};
+        catch(error)
+        {
+             console.log("something happenend in the repo layer");
+            throw{ error};
         }
     }
 }
