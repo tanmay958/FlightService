37e07f8d621bd39dd3e7e9d43b8f586b7632f44f
tanmay958 <studytime958@gmail.com>
2023-06-04 21:22:34 +0530

    handled error codes -  enum

--- src/middlewares/flight-middlewares.js
+++ src/middlewares/flight-middlewares.js
@@ -1,4 +1,5 @@
 // middleware are the function have the access to request bject,response object , next
+const {ClientErrorCodes} =  require("../utils/error-codes");
 const validateCreateFlight =  (req,res,next)=>{
     if(!req.body.flightNumber ||
         !req.body.AirplaneId ||
@@ -8,7 +9,7 @@ const validateCreateFlight =  (req,res,next)=>{
         !req.body.departureTime ||
         !req.body.price
         )
-        return res.status(400).json({
+        return res.status(ClientErrorCodes.BAD_REQUEST).json({
             data :{},
             success :  false,
             message :  "invalid request body for create a flight",
